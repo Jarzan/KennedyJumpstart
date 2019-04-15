c1684cd01e4205eb1d3a3e953437bcedfdf7d88c
Jari Jokela <jarijokela@icloud.com>
2019-04-15 21:24:25 +0300

    Created venv

---
+++ apps/01_hello_world/you_try/HelloWorldMe.py
@@ -0,0 +1,11 @@
+"""My first try on Hello World!"""
+
+print('-----------------------')
+print('      HELLO APP        ')
+print('-----------------------')
+print()
+
+name = input('What is your name? ')
+print()
+greeting = 'Nice to meet you, ' + name
+print(greeting)
