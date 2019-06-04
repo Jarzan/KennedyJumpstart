b5f456af1713d1dcd9e4191a2bb44a19aeb0dff9
Jari Jokela <jarijokela@icloud.com>
2019-06-04 21:20:35 +0300

    pip3 and requests added

--- apps/05_weather_client/you_try/MyWeatherClient.py
+++ apps/05_weather_client/you_try/MyWeatherClient.py
@@ -1,10 +1,12 @@
+import requests
+
+
 def main():
     #  print the header
     print_the_header()
     #  get zip code from user
     postinumero = input('Mille postinumerolle haluat sääennusteen? (01860)')
-    #  get html from web
-    get_html_from_web(postinumero)
+    html = get_html_from_web(postinumero)
     #  parse the html
     #  display the forecast
 
@@ -20,7 +22,12 @@ def print_the_header():
 def get_html_from_web(postinumero):
     url = 'https://www.wunderground.com/weather/fi/{}'.format(postinumero)
     # print(url)
-    requests
+    response = requests.get(url)
+    # print(response.status_code) # Tällä saadaan selville webbisivun status_code
+    #  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+    # print(response.text[0:250]) # Tämä antaa sivusta 250 ekaa merkkiä
+
+    return response.text
 
 def fetch_zip_code():
     pass
