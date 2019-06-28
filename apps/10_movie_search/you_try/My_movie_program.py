3b92a316beb311627a6bb30a917b984d3544fea7
Jari Jokela <jarijokela@icloud.com>
2019-06-28 20:00:54 +0300

    Beginning of Movie search app

---
+++ apps/10_movie_search/you_try/My_movie_program.py
@@ -0,0 +1,26 @@
+
+
+def main():
+    # print_header()
+    # movie_title()
+    # search_movie
+
+
+def print_header():
+    print('-------------------------------')
+    print('       Movie Search App')
+    print('-------------------------------')
+    print('')
+
+
+def movie_title():
+
+
+
+def search_movie():
+    # http://movie_service.talkpython.fm/api/search/
+
+
+if __name__ == '__main__':
+    main()
+
